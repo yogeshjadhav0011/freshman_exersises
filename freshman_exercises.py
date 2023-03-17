034606422d47509df979ddd02a3fc496e2796682
yogeshjadhav007 <yogeshsj@lentra.ai>
2023-03-17 15:43:19 +0530

    freshman_exercises

---
+++ freshman_exercises.py
@@ -0,0 +1,55 @@
+# freshman_exercises
+freshman exercises
+
+# sum of two number type1
+a = 1
+b = 3
+print(a+b)
+
+
+a = 2
+b = 3
+c = a+b
+print(c)
+
+
+# Using append()
+
+
+List=[]
+List.append(18) 
+List.append(7)
+List.append(1)
+print(List) 
+
+
+# Joining with empty separator
+list1 = ['g', 'e', 'e', 'k', 's']
+print("".join(list1))
+
+# Joining with string
+list1 = " geeks "
+print("".join(list1))
+
+
+#swap using third varible
+
+x = 18
+y = 7
+ 
+temp = x
+x = y
+y = temp
+ 
+print("Value of x:", x)
+print("Value of y:", y)
+
+#swap varible
+
+x = 18
+y = 7
+
+x, y = y, x
+ 
+print("Value of x:", x)
+print("Value of y:", y)
